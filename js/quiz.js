e261673a0cce4ae40d2bc4610bc6fa4aed9bb678
jan.ossowski <jan.ossowski@gmx.de>
2019-10-01 18:39:08 +0200

    first working version

--- js/quiz.js
+++ js/quiz.js
@@ -6,43 +6,33 @@ var currentQuestionNumber = 0;
 var rightAnswers = 0;
 var currentQuestion;
 
-var questions = [
-  {
-    "id": "1",
-    "question": "Wie sieht ein A aus?",
-    "answers": {
-      "A": "A",
-      "B": "B",
-      "C": "c",
-      "D": "D"
-    },
-    "right": "A"
-  },
-  {
-    "id": "2",
-    "question": "Wie sieht ein B aus?",
-    "answers": {
-      "A": "A",
-      "B": "B",
-      "C": "c",
-      "D": "D"
-    },
-    "right": "B"
-  }
-]
-
 function showNextQuestion() {
-  console.log("Loading question: " + currentQuestionNumber);
-  currentQuestion = questions[currentQuestionNumber];
+  if (currentQuestionNumber < questions.length) {
+    console.log("Loading question: " + currentQuestionNumber);
+    currentQuestion = questions[currentQuestionNumber];
 
-  $("#question_nbr").text(currentQuestionNumber + 1);
-  $("#question_text").text(currentQuestion.question);
-  $("#answer_a").text(currentQuestion.A);
-  $("#answer_b").text(currentQuestion.B);
-  $("#answer_c").text(currentQuestion.C);
-  $("#answer_d").text(currentQuestion.D);
+    $("#question_nbr").text(currentQuestionNumber + 1);
+    $("#question_text").text(currentQuestion.question);
+    $("#answer_a").text(currentQuestion.answers.A);
+    $("#answer_b").text(currentQuestion.answers.B);
+    $("#answer_c").text(currentQuestion.answers.C);
+    $("#answer_d").text(currentQuestion.answers.D);
+
+    return true;
+  } else {
+    console.log("no more questions");
+    return false;
+  }
 }
 
 function getRightAnswer() {
   return currentQuestion.right;
+}
+
+function incrementCurrentQuestionNumber() {
+  currentQuestionNumber = currentQuestionNumber + 1;
+}
+
+function incrementRightAnswers() {
+  rightAnswers = rightAnswers + 1;
 }
